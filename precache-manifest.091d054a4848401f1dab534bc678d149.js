d7669c18ac65f7cc67064f926053507272799bb1
Joshua T J <joshuajose312@gmail.com>
2022-02-05 17:05:04 +0530

    Updates

---
+++ precache-manifest.091d054a4848401f1dab534bc678d149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117eacccdc42a2558a4217c86bfeb1c1",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "059e40f5ed8b33da36af",
+    "url": "/todoapp/static/css/main.27fa0f64.chunk.css"
+  },
+  {
+    "revision": "7270c9dcb557a2ce04c0",
+    "url": "/todoapp/static/js/2.c840a184.chunk.js"
+  },
+  {
+    "revision": "059e40f5ed8b33da36af",
+    "url": "/todoapp/static/js/main.c7a95345.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
